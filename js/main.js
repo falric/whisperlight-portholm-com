4604acdf7d203b26fc042440c26f5bad462db501
Rickard Falk <rickard.falk@gmail.com>
2020-07-25 21:55:58 +0200

    Added styled console message

---
+++ js/main.js
@@ -0,0 +1,4 @@
+console.log(
+  "%c WHISPERLIGHT CORPORATION MAINFRAME",
+  "font-family: 'anuratiregular', 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;font-weight: bold; font-size: 1.5rem;color: #44658F;"
+);
