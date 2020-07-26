ce032271c8fa9e6ec300cd1357de58214f0c9ea8
Rickard Falk <rickard.falk@gmail.com>
2020-07-26 09:06:43 +0200

    Added terminal to site

--- js/main.js
+++ js/main.js
@@ -1,4 +1,1 @@
-console.log(
-  "%c WHISPERLIGHT CORPORATION MAINFRAME",
-  "font-family: 'anuratiregular', 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;font-weight: bold; font-size: 1.5rem;color: #44658F;"
-);
+require("./terminal");
---
+++ js/terminal.js
@@ -0,0 +1,77 @@
+(function (window, document, undefined) {
+  const { Terminal } = require("xterm");
+  const term = new Terminal();
+  term.open(document.getElementById("terminal"));
+
+  let command = "";
+
+  function runFakeTerminal() {
+    if (term._initialized) {
+      return;
+    }
+    term._initialized = true;
+
+    term.prompt = () => {
+      term.write("\r\n$ ");
+    };
+
+    term.onData(handleInput);
+
+    term.writeln("\x1b[1;36mWhisperlight Corporation Mainframe");
+    term.writeln("\x1b[37m");
+
+    prompt(term);
+  }
+
+  function prompt(term) {
+    term.write("\r\n$ ");
+  }
+
+  function handleInput(e) {
+    switch (e) {
+      case "\r": // Enter
+        // Handle entered command
+        handleCommand(command);
+        // Clear command
+        command = "";
+        // Show prompt
+        prompt(term);
+        break;
+      case "\u0003": // Ctrl+C
+        // Clear command
+        command = "";
+        // Show prompt
+        prompt(term);
+        break;
+      case "\u007F": // Backspace (DEL)
+        // Remove last character in command
+        command = command.substr(0, command.length - 1);
+        // Do not delete the prompt
+        if (term._core.buffer.x > 2) {
+          term.write("\b \b");
+        }
+        break;
+      default:
+        // Add to command
+        command += e;
+        // Print entered character
+        term.write(e);
+    }
+  }
+
+  function handleCommand(command) {
+    switch (command) {
+      case "":
+        break;
+      case "Login":
+      case "login":
+        term.write("\r\nLogin…");
+        break;
+      default:
+        term.write("\r\nSyntax Error");
+        break;
+    }
+  }
+
+  runFakeTerminal();
+})(window, document);
